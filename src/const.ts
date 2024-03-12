6c878b5f7743a39fc5bbd062344f767eec5d3895
Yonis Dideh <74731447+yd-915@users.noreply.github.com>
2024-03-12 12:53:37 -0400

    Update const.ts

--- src/const.ts
+++ src/const.ts
@@ -3,11 +3,11 @@ import { NDKKind } from "@nostr-dev-kit/ndk";
 export const GOAL = 9041 as NDKKind;
 export const SUPPORT = 7001 as NDKKind;
 export const HEYA_PUBKEY =
-  "5d1e8c763e3daa5e161b0bafc447db7a0ffa23bf6a3865fe39f388cf4e30f63b";
+  "npub192hs7rha7n0ar5xq7ulyyln9vlspyhfwrr8ln0qsgl3yjk5pe6kqc8sgwm";
 export const DEFAULT_RELAYS = [
-  "wss://frens.nostr1.com",
-  "wss://nos.lol",
-  "wss://relay.damus.io",
-  "wss://nostr.mom",
-  "wss://relay.nostr.band",
+  "wss://relay.current.fyi",
+  "wss://nostr.bitcoiner.social",
+  "wss://nos.lol/",
+  "wss://relay.nostr.info",
+  "wss://relay.snort.social",
 ];
