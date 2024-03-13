ca92688971d5e7e3ab3ff202f372ecbc9b40da87
Yonis Dideh <74731447+yd-915@users.noreply.github.com>
2024-03-13 01:00:38 -0400

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -20,29 +20,29 @@ export default function Home() {
   const navigate = useNavigate();
   const [session] = useSession();
   const isLoggedOut = session === null;
-  const { events: supporters } = useSupporters(HEYA_PUBKEY);
-  const { events } = useEvents(
-    [
-      {
-        kinds: [GOAL],
-        ids: [
-          "bd3b899997cd4ce115532a84eabe598bb7547cab8f44b06812b2306d64761096",
-          "9b734bc67402c034857ec3f2ecd8e74d61d38f46505067c5e53986cf70a0c4f6",
-          "060f4f06455ee0a87db48f7d5f23b532bcc133cea7dd3bc9f2a20226f1bf2705",
-        ],
-      },
-      {
-        kinds: [GOAL],
-        authors: supporters.map((ev) => ev.pubkey),
-      },
-    ],
-    {
-      cacheUsage: NDKSubscriptionCacheUsage.PARALLEL,
-    },
-  );
-  const featuredGoals = useMemo(() => {
-    return dedupeByPubkey(events);
-  }, [events]);
+//  const { events: supporters } = useSupporters(HEYA_PUBKEY);
+  //const { events } = useEvents(
+   // [
+     // {
+       // kinds: [GOAL],
+       // ids: [
+         // "bd3b899997cd4ce115532a84eabe598bb7547cab8f44b06812b2306d64761096",
+         // "9b734bc67402c034857ec3f2ecd8e74d61d38f46505067c5e53986cf70a0c4f6",
+         // "060f4f06455ee0a87db48f7d5f23b532bcc133cea7dd3bc9f2a20226f1bf2705",
+       // ],
+     // },
+     // {
+       // kinds: [GOAL],
+      //  authors: supporters.map((ev) => ev.pubkey),
+     // },
+   // ],
+   // {
+     // cacheUsage: NDKSubscriptionCacheUsage.PARALLEL,
+   // },
+  // );
+//  const featuredGoals = useMemo(() => {
+  //  return dedupeByPubkey(events);
+ // }, [events]);
 
   function createZapGoal() {
     navigate(NEW_GOAL);
@@ -51,9 +51,9 @@ export default function Home() {
   return (
     <Stack align="center" spacing={20} mt={12}>
       <CallToAction
-        label="Lightning Fundraisers"
-        title="Empower Dreams"
-        description="Fund your open-source projects with Bitcoin Lightning. Fundraising is instantly available without any fees."
+        label="Open-Source Supporters Unite!"
+        title="Empower the indie dev!"
+        description="Fund your open-source projects or others with Bitcoin Lightning. Fundraising is instantly available without any fees."
         ctaText="Start a fund"
         ctaAction={createZapGoal}
       />
@@ -66,7 +66,7 @@ export default function Home() {
       <CallToAction
         label="Almost unreal"
         title="Zero fees. Multiple recipients. Instant availability. It's kind of amazing!"
-        description="There’s no party in the middle so all funds go directly to YOU future innovator, LoghtningCharge uses the bitcoin lightning network to send payments anywhere in the world for near-zero cost. This makes it easy to send $1,000 or $0.01."
+        description="There’s no party in the middle so all funds go directly to YOU future innovator, BitHustle uses the bitcoin lightning network to send payments anywhere in the world for near-zero cost. This makes it easy to send $1,000 or $0.01."
         ctaText="Start a fund"
         ctaAction={createZapGoal}
       />
@@ -76,7 +76,7 @@ export default function Home() {
       <CallToAction
         label="Your dreams await"
         title="Get the boost you need. Start a fund for your own project or someone else."
-        description="LightningCharge! is an open-source project made possible by the folks at OpenSats."
+        description="BitHustle is an open-source project made possible by the folks at OpenSats."
         ctaText="Start a fund"
         ctaAction={createZapGoal}
       />
