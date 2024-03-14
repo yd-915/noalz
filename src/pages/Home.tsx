889e1eacc3469e80e6911c3809738cf73fe85bd8
Yonis Dideh <74731447+yd-915@users.noreply.github.com>
2024-03-14 01:19:55 -0400

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,10 @@
-import { useMemo } from "react";
+//import { useMemo } from "react";
 import { Stack } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
 import { NDKSubscriptionCacheUsage } from "@nostr-dev-kit/ndk";
 
 import CallToAction from "@goalz/components/CallToAction";
-import FeaturedGoals from "@goalz/components/FeaturedGoals";
+//import FeaturedGoals from "@goalz/components/FeaturedGoals";
 import Features from "@goalz/components/Features";
 import Support from "@goalz/components/Support";
 import Link from "@goalz/components/Link";
@@ -14,34 +14,34 @@ import { GOAL, HEYA_PUBKEY } from "@goalz/const";
 
 import useEvents from "@ngine/nostr/useEvents";
 import useSession from "@ngine/hooks/useSession";
-import { dedupeByPubkey } from "@ngine/utils";
+//import { dedupeByPubkey } from "@ngine/utils";
 
 export default function Home() {
   const navigate = useNavigate();
   const [session] = useSession();
   const isLoggedOut = session === null;
   const { events: supporters } = useSupporters(HEYA_PUBKEY);
-  const { events } = useEvents(
-    [
-     {
-        kinds: [GOAL],
-        ids: [
-          "nostr:nevent1qqsfhus8fz0sf6q7mhn7v78ggarcy5dm2dvrx8l8puut2f2749kvm2spzamhxue69uhhyetvv9ujucm4wfex2mn59en8j6gzyptswsrlvxfqhdv9k0n2zsvpy7dp6w6hnrzf4xdjcxq7m8ehn3h8kwzxwq6",
-          "nostr:nevent1qqst3k6a7z5pecuvf8c8cuuen4lgtc4kc9kur7axmqftpuc6tauy4sgppemhxue69uhkummn9ekx7mp0qgs9wp6q0aseyza4ske7dg2psyne58fm27vvfx5ektqcrmvlx7wxu7cl6wnxv",
-        ],
-      },
-      {
-        kinds: [GOAL],
-        authors: supporters.map((ev) => ev.pubkey),
-      },
-    ],
-    {
-      cacheUsage: NDKSubscriptionCacheUsage.PARALLEL,
-    },
-   );
-  const featuredGoals = useMemo(() => {
-    return dedupeByPubkey(events);
-  }, [events]);
+  //const { events } = useEvents(
+    //[
+     //{
+        //kinds: [GOAL],
+        //ids: [
+          //"nostr:nevent1qqsfhus8fz0sf6q7mhn7v78ggarcy5dm2dvrx8l8puut2f2749kvm2spzamhxue69uhhyetvv9ujucm4wfex2mn59en8j6gzyptswsrlvxfqhdv9k0n2zsvpy7dp6w6hnrzf4xdjcxq7m8ehn3h8kwzxwq6",
+          //"nostr:nevent1qqst3k6a7z5pecuvf8c8cuuen4lgtc4kc9kur7axmqftpuc6tauy4sgppemhxue69uhkummn9ekx7mp0qgs9wp6q0aseyza4ske7dg2psyne58fm27vvfx5ektqcrmvlx7wxu7cl6wnxv",
+        //],
+      //},
+      //{
+        //kinds: [GOAL],
+        //authors: supporters.map((ev) => ev.pubkey),
+      //},
+    //],
+    //{
+      //cacheUsage: NDKSubscriptionCacheUsage.PARALLEL,
+    //},
+   //);
+  //const featuredGoals = useMemo(() => {
+    //return dedupeByPubkey(events);
+  //}, [events]);
 
   function createZapGoal() {
     navigate(NEW_GOAL);
