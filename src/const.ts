c9eb3c1713b37672006dc598fb22facacd99cddd
Yonis Dideh <74731447+yd-915@users.noreply.github.com>
2024-03-15 00:31:48 -0400

    Update const.ts

--- src/const.ts
+++ src/const.ts
@@ -3,7 +3,7 @@ import { NDKKind } from "@nostr-dev-kit/ndk";
 export const GOAL = 9041 as NDKKind;
 export const SUPPORT = 7001 as NDKKind;
 export const HEYA_PUBKEY =
-  "npub192hs7rha7n0ar5xq7ulyyln9vlspyhfwrr8ln0qsgl3yjk5pe6kqc8sgwm";
+  "5d1e8c763e3daa5e161b0bafc447db7a0ffa23bf6a3865fe39f388cf4e30f63b";
 export const DEFAULT_RELAYS = [
   "wss://relay.current.fyi",
   "wss://nostr.bitcoiner.social",
